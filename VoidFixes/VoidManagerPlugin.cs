8075dc0349c805e0d9d452fde2cb1ce8fb9a472f
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-06-21 13:25:00 -0700

    Added 18107 to author list.

--- VoidFixes/VoidManagerPlugin.cs
+++ VoidFixes/VoidManagerPlugin.cs
@@ -6,15 +6,13 @@ namespace VoidFixes
     {
         public override MultiplayerType MPType => MultiplayerType.Client;
 
-        public override string Author => "Dragon";
+        public override string Author => "Dragon, 18107";
 
         public override string Description => "Fixes or provides the ability to fix bugs. Client-Side";
     }
 }
-// Bug fix idea: fix fighters not carring about distance or obstacles when shooting players. - reqires removing current damage system and 
+// Bug fix idea: fix fighters not carring about distance or obstacles when shooting players. - reqires removing current damage system and allowing fighter projectiles to damage player
 //
-// Supply drops don't delete when leaving sector
-// Spawner Prefabs don't delete when leaving sector
 //
 //
 //
