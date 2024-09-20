37fae27d4a5063220d1505ca8479a348c017c9db
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-09-20 14:58:24 -0700

    Removed EngineeringDoorFix, as the bug has been fixed in vanilla.

--- VoidFixes/BepInPlugin.cs
+++ VoidFixes/BepInPlugin.cs
@@ -21,7 +21,6 @@ namespace VoidFixes
             DestroySpawnersOnLeavePatch = Config.Bind("Patches", "DestroySpawnersOnLeave", true, "Host-side, effects networking. Destroys spawners on sector leave (they don't normally get destroyed, just hidden).");
             DestroImpactFXOnLeavePatch = Config.Bind("Patches", "DestroyImpactFXOnLeave", true, "Client-side, Destroys old impactFX on sector leave (they don't normally get destroyed until there's 1024 of a given effect or the session ends).");
             FinalizePerkTreeNullPatch = Config.Bind("Patches", "FinalizePerkTreeNull", true, "Client-Side, Fixes exception which occurs when a player character is loaded.");
-            FrigateEngineeringDoorFix = Config.Bind("Patches", "FrigateEngineeringDoorFix", true, "Client-Side, Fixes frigate engineering door being closed when joining a game.");
             SetCarriableActivePatch = Config.Bind("Patches", "SetCarriableActive", true, "Client-Side, Attempts to fix picking up invisible items which then locks pickup ability. Try '/fix pickup' command if a similar issue is found.");
             CharacterJoinCastsPatch = Config.Bind("Patches", "CharacterJoinCasts", true, "Client-Side, Fixes exceptions which occur when a player character is loaded.");
             WaitDurationNullPatch = Config.Bind("Patches", "WaitDurationNull", true, "Client-Side, Fixes exception which occurs if a reclaimer timer starts while jumping out of the current sector.");
@@ -39,7 +38,6 @@ namespace VoidFixes
             internal static ConfigEntry<bool> DestroySpawnersOnLeavePatch;
             internal static ConfigEntry<bool> DestroImpactFXOnLeavePatch;
             internal static ConfigEntry<bool> FinalizePerkTreeNullPatch;
-            internal static ConfigEntry<bool> FrigateEngineeringDoorFix;
             internal static ConfigEntry<bool> SetCarriableActivePatch;
             internal static ConfigEntry<bool> CharacterJoinCastsPatch;
             internal static ConfigEntry<bool> WaitDurationNullPatch;
