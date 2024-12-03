1bf22c70232df71955f808c0a1d77cbba85257f9
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-02 16:07:28 -0800

    Added dead code for archive.

---
+++ VoidFixes/Patches/OfflineHostDisconnect.cs
@@ -0,0 +1,22 @@
+﻿using CG.GameLoopStateMachine;
+using CG.GameLoopStateMachine.GameStates;
+using HarmonyLib;
+using Photon.Pun;
+
+namespace VoidFixes.Patches
+{
+    // The start of an investigation into keeping the session intact in the event of a connection loss.
+    // Deemed insanely difficult, so the next step is offline mode and saving
+    [HarmonyPatch(typeof(GSPhotonDisconnected), "OnEnter")]
+    internal class OfflineHostDisconnect
+    {
+        static bool Prefix(IState previousState)
+        {
+            if (previousState is GSIngame && PhotonNetwork.IsMasterClient)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
